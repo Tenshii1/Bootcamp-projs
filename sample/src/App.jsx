b9b4f03cdba81f2e74e86449a0089f0bd2fa2548
Tenshii1 <146460793+Tenshii1@users.noreply.github.com>
2025-12-08 21:17:04 +0800

    fixes

--- sample/src/App.jsx
+++ sample/src/App.jsx
@@ -2,22 +2,23 @@ import { Link } from 'react-router-dom';
 
   function App() {
     return (
-      <div className="w-screen h-screen items-center justify-center bg-gray-500 flex">
-          <div className='h-[500px] w-[400px] p-5 border-4 border-white bg-blue-200 shadow-lg items-center rounded-lg flex flex-col gap-10'>
-            <h1 className='text-4xl font-bold text-orange-300 mg-10'>Login</h1>
+      <div className="w-screen h-screen items-center justify-center bg-[#1e293b] flex">
+          <div className='h-[500px] w-[400px] p-5 border-4 border-white bg-[#334155] shadow-lg items-center rounded-lg flex flex-col gap-10'>
+            <h1 className='text-4xl font-bold text-[#38bdf8] mg-10'>Login</h1>
               <input className='h-10 bg-white w-full p-4 rounded-md'
                 type='text'
                 placeholder='User'
               />
               <input className='h-10 bg-white w-full p-4 rounded-md'
-                type='text'
+                type='password'
                 placeholder='Password'
               />
-              <Link to="/Landing" className="flex justify-center items-center bg-transparent shadow-lg border-2 text-blue-400 w-full h-10  p-2 rounded-md focus:outline-none hover:bg-blue-400 hover:text-white font-bold ml-1 transition duration-300"
+              <Link to="/Landing" className="flex justify-center items-center bg-transparent shadow-sm border-2 text-[#14b8a6] w-full h-10 
+              p-2 rounded-md focus:outline-none hover:bg-[#0d9488] hover:text-white hover:shadow-lg font-bold ml-1 transition duration-300"
                 >Login</Link>
-              <h3 className='text-black text-sm'>
+              <h3 className='text-[#e2e8f0]'>
                 Don't have an account?
-                <Link to="/Landing" className="text-red-400 hover:text-red-100 font-bold ml-1 transition duration-300"
+                <Link to="/Landing" className="text-[#f59e0b] hover:text-[#0ea5e9] font-bold ml-1 transition duration-300"
                 >Sign Up</Link>
               </h3>
               
--- sample/src/Landing.jsx
+++ sample/src/Landing.jsx
@@ -1,24 +1,44 @@
-// src/LandingPage.jsx
-import React from 'react';
+// src/Landing.jsx
+import React from "react";
+import LandBG from "./assets/image/harbor.jpg";
 
 function Landing() {
   return (
-    <div className=" bg-blue-300 p-8 text-center min-h-screen flex flex-col items-center justify-center">
-      <div class="menu" className='fixed top-3 w-full max-w-4xl'>
-        <nav className="bg-white rounded-md shadow-md p-4 mb-8">
-          <ul className="flex justify-center gap-15 space-x-8 text-lg font-semibold text-emerald-700">
-            <li><a href="#home" className="hover:text-emerald-900">Home</a></li>
-            <li><a href="#about" className="hover:text-emerald-900">About</a></li>
-            <li><a href="#services" className="hover:text-emerald-900">Services</a></li>
-            <li><a href="#contact" className="hover:text-emerald-900">Contact</a></li>
+    <div
+      className="p-8 text-center min-h-screen flex flex-col items-center justify-center"
+      style={{
+        backgroundImage: `url(${LandBG})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      
+      <div className="menu fixed top-3 w-full max-w-4xl">
+        <nav className="bg-white-5 rounded-md shadow-md p-4 mb-8">
+          <ul className="flex justify-center gap-6 space-x-8 text-lg font-semibold text-emerald-700">
+            <li>
+              <a href="#home" className="hover:bg-blue-400 hover:text-white p-2 rounded-md">Home</a>
+            </li>
+            <li>
+              <a href="#about" className="hover:bg-blue-400 hover:text-white p-2 rounded-md">About</a>
+            </li>
+            <li>
+              <a href="#services" className="hover:bg-blue-400 hover:text-white p-2 rounded-md">Services</a>
+            </li>
+            <li>
+              <a href="#contact" className="hover:bg-blue-400 hover:text-white p-2 rounded-md">Contact</a>
+            </li>
           </ul>
         </nav>
       </div>
-      <h1 className="text-5xl font-extrabold text-emerald-700 flex justify-center items-center bg-transparent shadow-lg border-2 w-200 h-50  p-16 rounded-md focus:outline-none">
+
+      <h1 className="text-5xl font-extrabold text-white shadow-lg border-2 p-16 rounded-md bg-black/40">
         Welcome to the Landing Page! 🚀
       </h1>
+
     </div>
-  );
+  ); 
 }
 
 export default Landing;
